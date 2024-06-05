dab1ab4b0891c12519cd1a69050f80103663b02e
Greg Tuzov <greg.tuzov@gmail.com>
2024-06-05 22:11:45 +0300

    Add docker related files

---
+++ run_docker.sh
@@ -0,0 +1,3 @@
+docker run -v /Users/grigorii_tuzov/NastyaDiplom/oemer_rest_api/oemer_docker/image.jpeg:/input_image.jpeg \
+  -v /Users/grigorii_tuzov/NastyaDiplom/oemer_rest_api/oemer_docker/output_dir:/output_dir \
+  oemer -o /output_dir /input_image.jpeg
